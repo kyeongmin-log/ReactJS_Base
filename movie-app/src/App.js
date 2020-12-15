8533e75b6b6e09a7b28784d520642a3ee95ad000
kyeongmin-log <dotcom7860@gmail.com>
2020-12-15 17:35:24 +0900

    jsx개념 및 props 사용

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+//es6에서 나온 문법. props의 내부 속성에 접근한다. 즉, props.fav === {fav}
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+//<Food />와 같은 기능을 jsx(HTML + javascript)라고 한다. jsx는 react에서 나오는 특징이다.
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Food fav="chicken" />
     </div>
   );
 }
