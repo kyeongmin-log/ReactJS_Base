5563515610959ffb0d5c2fc5b515d852af9a1386
kyeongmin-log <dotcom7860@gmail.com>
2020-12-19 13:28:01 +0900

    add genres and styles and cut summary

--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -3,14 +3,21 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 //es6 문법. ex)원래 props를 가져와서 props.id 이런 식으로 썼지만 {id}라는 문법을 통해 바로 id로 쓸 수 있다.
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
@@ -22,6 +29,7 @@ Movie.prototype = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
